4f32d43eba6ccb7530743f616a880088deddcc83
Dmitriy877 <dmitriybukin05@gmail.com>
2025-03-19 22:28:25 +0300

    Update Lesson 2

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,31 @@
 import os
+from dotenv import load_dotenv
+from environs import env
+load_dotenv()
+
+DATABES_NAME = os.environ["NAME"]
+DATABES_USER = os.environ["USER"]
+DATABES_PASSWORD = os.environ["PASSWORD"]
+DATABES_SECRET_KEY = os.environ["SECRET_KEY"]
+DEBUG = env.bool("DEBUG", False)
+
+print(DEBUG)
+print(type(DEBUG))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': 'checkpoint.devman.org',
         'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'NAME': DATABES_NAME,
+        'USER': DATABES_USER,
+        'PASSWORD': DATABES_PASSWORD,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
-
-DEBUG = True
+SECRET_KEY = DATABES_SECRET_KEY
 
 ROOT_URLCONF = 'project.urls'
 
