b32d183208ae4ddfc438c2df47ffdb47b9f9c03f
Dmitriy877 <dmitriybukin05@gmail.com>
2025-03-20 19:13:37 +0300

    use only envorons

--- project/settings.py
+++ project/settings.py
@@ -3,51 +3,51 @@ from dotenv import load_dotenv
 from environs import env
 load_dotenv()
 
-DATABES_NAME = os.environ["NAME"]
-DATABES_USER = os.environ["USER"]
-DATABES_PASSWORD = os.environ["PASSWORD"]
-DATABES_SECRET_KEY = os.environ["SECRET_KEY"]
+DATABES_NAME = env("NAME")
+DATABES_USER = env("USER")
+DATABES_PASSWORD = env("PASSWORD")
+DATABES_SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", False)
 
 print(DEBUG)
 print(type(DEBUG))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': DATABES_NAME,
-        'USER': DATABES_USER,
-        'PASSWORD': DATABES_PASSWORD,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': 'checkpoint.devman.org',
+#         'PORT': '5434',
+#         'NAME': DATABES_NAME,
+#         'USER': DATABES_USER,
+#         'PASSWORD': DATABES_PASSWORD,
+#     }
+# }
 
-INSTALLED_APPS = ['datacenter']
+# INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = DATABES_SECRET_KEY
+# SECRET_KEY = DATABES_SECRET_KEY
 
-ROOT_URLCONF = 'project.urls'
+# ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-    },
-]
+# ALLOWED_HOSTS = ['*']
 
 
-USE_L10N = True
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+#         'APP_DIRS': True,
+#     },
+# ]
 
-LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Europe/Moscow'
+# USE_L10N = True
 
-USE_TZ = True
+# LANGUAGE_CODE = 'ru-ru'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+# TIME_ZONE = 'Europe/Moscow'
+
+# USE_TZ = True
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
